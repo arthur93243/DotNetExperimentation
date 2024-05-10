33a84bc6df9357204bcc08407c64ed4077a0090e
Arthur <arthur93243@gmail.com>
2024-05-10 15:50:10 +0800

    new: redis sample

---
+++ RedisSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+
+namespace RedisSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Redis Sample!");
+
+            RedisSimpleTest();
+        }
+
+        static void RedisSimpleTest()
+        {
+            // ConfigurationOptions
+            ConfigurationOptions config = ConfigurationOptions.Parse("127.0.0.1:6379");
+            ConnectionMultiplexer conn = ConnectionMultiplexer.Connect(config.ToString());
+
+            IDatabase db = conn.GetDatabase();
+
+            // Set
+            string value = "Hello World";
+            db.StringSet("Test", value);
+
+            // Get
+            RedisValue test = db.StringGet("Test");
+
+            Console.WriteLine(test);
+        }
+    }
+}
